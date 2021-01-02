98f18100e8b55c28c5f29f126e7a3486b370231a
Berenas54 <haravoy1998@mail.ru>
2021-01-03 01:51:48 +0300

    init. start Portfolio

---
+++ src/Contacts/Contacts.js
@@ -0,0 +1,23 @@
+import s from './Contacts.module.css';
+import sCon from "./../common/styles/container.module.css"
+
+
+function Contacts() {
+    return (
+        <div className={s.contactsBlock}>
+            <div className={`${sCon.container} ${s.contactsContainer}`}>
+<h2>Contacts</h2>
+<div>
+    <form action={"#!"}>
+       <div><input placeholder={"Name"}/></div>
+       <div><input  placeholder={"E-mail"}/></div>
+        <div> <textarea placeholder={"Your messages"}></textarea></div>
+        <button>SEND MESSAGE</button>
+    </form>
+</div>
+            </div>
+        </div>
+    );
+}
+
+export default Contacts;
